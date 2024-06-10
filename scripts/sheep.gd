6d0d5e35c18d70836dcbaea3f42de03f38079fe6
kmhatch <keira.lindgren@yahoo.com>
2024-06-10 17:50:45 -0500

    Fixed movement

--- scripts/sheep.gd
+++ scripts/sheep.gd
@@ -7,14 +7,16 @@ func _physics_process(delta):
 	var direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * speed
 	
-	match str(direction):
-		"(0, 1)":
-			animated_sprite.frame = 0
-		"(0, -1)":
-			animated_sprite.frame = 1
-		"(1, 0)":
-			animated_sprite.frame = 2
-		"(-1, 0)":
-			animated_sprite.frame = 3
+	if direction.y > 0:
+		animated_sprite.frame = 0
+	elif direction.y < 0:
+		animated_sprite.frame = 1
+	
+	if direction.x > 0:
+		animated_sprite.frame = 2
+	elif direction.x < 0:
+		animated_sprite.frame = 3
+		
+	print(direction)
 	
 	move_and_slide()
