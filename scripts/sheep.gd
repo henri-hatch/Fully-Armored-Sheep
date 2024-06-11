19a158a64f31826312278d8f15dd9e90dd7670a4
henri-hatch <henrihatch@gmail.com>
2024-06-11 18:06:50 -0500

    Added walking + some more animation fixes

--- scripts/sheep.gd
+++ scripts/sheep.gd
@@ -1,9 +1,14 @@
 extends CharacterBody2D
 
-var speed = 125
+var speed = 100
 @onready var animated_sprite = $AnimatedSprite2D
 
 func _physics_process(delta):
+	if Input.is_action_pressed("walk"):
+		speed = 50
+	else:
+		speed = 100
+	
 	var direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * speed
 	
