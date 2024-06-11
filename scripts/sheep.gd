de49ea86f208af6db284a076454be6eca805524c
henri-hatch <henrihatch@gmail.com>
2024-06-11 17:56:04 -0500

    Animation fixes

--- scripts/sheep.gd
+++ scripts/sheep.gd
@@ -7,14 +7,16 @@ func _physics_process(delta):
 	var direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * speed
 	
-	if direction.y > 0:
-		animated_sprite.play("WalkDown")
-	elif direction.y < 0:
-		animated_sprite.play("WalkUp")
-		
-	if direction.x > 0:
-		animated_sprite.play("WalkRight")
-	elif direction.x < 0:
-		animated_sprite.play("WalkLeft")
+	if direction != Vector2.ZERO:
+		if direction.x > 0:
+			animated_sprite.play("WalkRight")
+		elif direction.x < 0:
+			animated_sprite.play("WalkLeft")
+		elif direction.y > 0:
+			animated_sprite.play("WalkDown")
+		elif direction.y < 0:
+			animated_sprite.play("WalkUp")
+	elif direction == Vector2.ZERO:
+		animated_sprite.frame = 1
 	
 	move_and_slide()
