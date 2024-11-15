2a8be3c41ecc4106e62b0aaaa252aebe51bc24ff
kmhatch <keira.lindgren@yahoo.com>
2024-11-14 23:43:45 -0600

    Added cutscene (forgot to stage changes the first time lol)

--- scripts/openingCutscene.gd
+++ scripts/openingCutscene.gd
@@ -1,6 +1,6 @@
-extends Node2D
+extends DialogicBackground
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	GlobalVariables.player_can_move = true
+	GlobalVariables.player_can_move = false
