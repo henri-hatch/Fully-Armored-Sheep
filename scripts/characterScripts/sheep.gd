f0866550b67c7212de0fb40c0251060e640aec70
kmhatch <keira.lindgren@yahoo.com>
2024-11-25 23:46:41 -0600

    Add animation player and attack animations for player

--- scripts/characterScripts/sheep.gd
+++ scripts/characterScripts/sheep.gd
@@ -1,7 +1,12 @@
 extends CharacterBody2D
 
 var speed = 100
-@onready var animated_sprite = $AnimatedSprite2D
+var direction = Vector2.ZERO
+var lastAnimDir: String = "Down"
+var isAttacking: bool = false
+@onready var animations = $AnimationPlayer
+@onready var weapon = $Weapon
+
 
 func _physics_process(delta):
 	if GlobalVariables.player_can_move == true:
@@ -10,19 +15,31 @@ func _physics_process(delta):
 		else:
 			speed = 100
 		
-		var direction = Input.get_vector("left", "right", "up", "down")
-		velocity = direction * speed
+		var playerDirection = Input.get_vector("left", "right", "up", "down")
+		velocity = playerDirection * speed
 		
-		if direction != Vector2.ZERO:
-			if direction.x > 0:
-				animated_sprite.play("WalkRight")
-			elif direction.x < 0:
-				animated_sprite.play("WalkLeft")
-			elif direction.y > 0:
-				animated_sprite.play("WalkDown")
-			elif direction.y < 0:
-				animated_sprite.play("WalkUp")
-		elif direction == Vector2.ZERO:
-			animated_sprite.frame = 1
+		if Input.is_action_pressed("attack"):
+			animations.play("attack" + lastAnimDir)
+			isAttacking = true
+			weapon.visible = true
+			await animations.animation_finished
+			isAttacking = false
+		
+		if playerDirection != direction:
+			if playerDirection.x > 0:
+				animations.play("walkRight")
+				lastAnimDir = "Right"
+			elif playerDirection.x < 0:
+				animations.play("walkLeft")
+				lastAnimDir = "Left"
+			elif playerDirection.y > 0:
+				animations.play("walkDown")
+				lastAnimDir = "Down"
+			elif playerDirection.y < 0:
+				animations.play("walkUp")
+				lastAnimDir = "Up"
+		elif playerDirection == direction:
+			animations.stop()
+			weapon.visible = false
 		
 		move_and_slide()
