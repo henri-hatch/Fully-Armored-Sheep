21b2e9e8087410afa14c5b83db7617c6552f1686
kmhatch <keira.lindgren@yahoo.com>
2024-11-29 19:15:39 -0600

    Added Gluttony sprites and animations, edited pause to force all characters to stop movement, added Gluttony to cutscene

--- scripts/characterScripts/snake.gd
+++ scripts/characterScripts/snake.gd
@@ -5,17 +5,18 @@ var player_chase = false
 var player = null
 
 func _physics_process(delta):
-	if player_chase:
-		position += (player.position - position)/speed
-		
-		$AnimatedSprite2D.play("Right")
-		
-		if(player.position.x - position.x) < 0:
-			$AnimatedSprite2D.flip_h = true
+	if GlobalVariables.characters_can_move == true:
+		if player_chase:
+			position += (player.position - position)/speed
+			
+			$AnimatedSprite2D.play("Right")
+			
+			if(player.position.x - position.x) < 0:
+				$AnimatedSprite2D.flip_h = true
+			else:
+				$AnimatedSprite2D.flip_h = false
 		else:
-			$AnimatedSprite2D.flip_h = false
-	else:
-		$AnimatedSprite2D.play("RightIdle")
+			$AnimatedSprite2D.play("RightIdle")
 func _on_detection_area_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
 	player=body
 	player_chase=true
--- scripts/cutscenes/openingCutscene/slothGluttony.gd
+++ scripts/cutscenes/openingCutscene/slothGluttony.gd
@@ -1,11 +1,13 @@
 extends DialogicBackground
 
 @onready var slothAnim = $Sloth/AnimationPlayer
+@onready var gluttonyAnim = $Gluttony/AnimationPlayer
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	slothAnim.play("idleRight")
+	gluttonyAnim.play("idleLeft")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
