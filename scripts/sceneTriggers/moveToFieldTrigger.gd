6a632ab8f16f3facaaa7da3d8802331abcaf5138
kmhatch <keira.lindgren@yahoo.com>
2024-12-05 15:37:26 -0600

    Added sign text

--- scripts/sceneTriggers/moveToFieldTrigger.gd
+++ scripts/sceneTriggers/moveToFieldTrigger.gd
@@ -1,7 +1,6 @@
 extends Area2D
 
 var entered = false
-signal fromTrainingGrounds
 
 func _on_body_entered(body: PhysicsBody2D):
 	if body.name == "Sheep":
@@ -10,5 +9,4 @@ func _on_body_entered(body: PhysicsBody2D):
 
 func _process(delta):
 	if entered:
-		emit_signal("fromTrainingGrounds")
 		get_tree().change_scene_to_file("res://scenes/main.tscn")
