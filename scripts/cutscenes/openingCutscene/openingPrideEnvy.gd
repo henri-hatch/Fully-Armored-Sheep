fba0487f739aa2cb72217fce9911f0503ac850c1
kmhatch <keira.lindgren@yahoo.com>
2024-12-07 17:53:55 -0600

    Added Envy sprites and animations, added to opening cutscene (opening cutscene finished!)

--- scripts/cutscenes/openingCutscene/openingPrideEnvy.gd
+++ scripts/cutscenes/openingCutscene/openingPrideEnvy.gd
@@ -1,7 +1,9 @@
 extends DialogicBackground
 
 @onready var prideAnim = $Pride/AnimationPlayer
+@onready var envyAnim = $Envy/AnimationPlayer
 
 
 func _ready():
 	prideAnim.play("idleRight")
+	envyAnim.play("idleLeft")
